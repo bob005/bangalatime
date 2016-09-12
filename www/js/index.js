e2e8774dcf83037f47266d209beb0fc9aff2fde2
projukti <sayantan.projukti@gmail.com>
2016-09-12 16:30:38 +0530

    Bangla Time 1.0.2

--- www/js/index.js
+++ www/js/index.js
@@ -207,8 +207,8 @@ function slide(hrf,direction) {
                     "duration"         :  300, // in milliseconds (ms), default 400
                     "slowdownfactor"   :    1, // overlap views (higher number is more) or no overlap (1). -1 doesn't slide at all. Default 4
                     //"slidePixels"      :  100, // optional, works nice with slowdownfactor -1 to create a 'material design'-like effect. Default not set so it slides the entire page.
-                    "iosdelay"         :  100, // ms to wait for the iOS webview to update before animation kicks in, default 60
-                    "androiddelay"     :  100, // same as above but for Android, default 70
+                    "iosdelay"         :  300, // ms to wait for the iOS webview to update before animation kicks in, default 60
+                    "androiddelay"     :  300, // same as above but for Android, default 70
                     "fixedPixelsTop"   :   44, // the number of pixels of your fixed header, default 0 (iOS and Android)
                     "fixedPixelsBottom":    0, // the number of pixels of your fixed footer (f.i. a tab bar), default 0 (iOS and Android)
                     'href'             :  hrf
@@ -220,8 +220,8 @@ function slide(hrf,direction) {
             window.plugins.nativepagetransitions.flip({
                     "direction"      : "right", // 'left|right|up|down', default 'right' (Android currently only supports left and right)
                     "duration"       :  300, // in milliseconds (ms), default 400
-                    "iosdelay"       :  100, // ms to wait for the iOS webview to update before animation kicks in, default 60
-                    "androiddelay"   :  100, // same as above but for Android, default 70
+                    "iosdelay"       :  300, // ms to wait for the iOS webview to update before animation kicks in, default 60
+                    "androiddelay"   :  300, // same as above but for Android, default 70
                     'href'           :  hrf
                 },
                 function () {
